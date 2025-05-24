c7d38a2d90ec19824de18933a1b6129e1b9fe6d9
じゅんや <j.nakayama.1234@gmail.com>
2025-05-24 23:54:40 +0900

    様々な変更

--- index.js
+++ index.js
@@ -10,19 +10,31 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
 app.get("/",(req,res) => {
-    res.render("portfolio/home");
+    res.render("portfolio/home",{
+        title:"ホームページ-junya",
+        description:"これはjunyaのポートフォリオサイトのホームページです。"
+    });
 });
 
 app.get("/skills",(req,res) => {
-    res.render("portfolio/skills");
+    res.render("portfolio/skills",{
+        title:"skills",
+        description:"これはjunyaのスキルを表示するページです。主に技術スタックを掲載しています。"
+    });
 });
 
 app.get("/works",(req,res) => {
-    res.render("portfolio/works");
+    res.render("portfolio/works",{
+        title:"実績一覧",
+        description:"これはjunyaの実績を表示するページです。制作したHPやWEBアプリを掲載しています。"
+    });
 });
 
 app.get("/yelpcamp",(req,res) => {
-    res.render("portfolio/works/yelpcamp");
+    res.render("portfolio/works/yelpcamp",{
+        title:"yelpcamp",
+        description:"これはjunyaが作成したWEBアプリ「yelpcamp」の詳細ページです。"
+    });
 });
 
 
